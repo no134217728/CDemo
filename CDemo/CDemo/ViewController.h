6fd5e068bcea5fb26926f21ab414cb050270cd7c
黃紋吸蜜 <no134217728@gmail.com>
2018-07-09 19:17:45 +0800

    feat(Global): initial the blank project

---
+++ CDemo/CDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CDemo
+//
+//  Created by Gelbhaubenkakadu on 2018/7/9.
+//  Copyright © 2018年 Apacer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CDemo/CDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CDemo
+//
+//  Created by Gelbhaubenkakadu on 2018/7/9.
+//  Copyright © 2018年 Apacer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
